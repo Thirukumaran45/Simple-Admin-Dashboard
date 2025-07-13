d35b03d348719c4bc2bcbab9ca9f199cf8197235
THIRU KUMARAN N R <125803167+Thirukumaran45@users.noreply.github.com>
2025-07-13 19:56:30 +0530

    resolved the pdf download problem in the production

--- lib/contant/constant.dart
+++ lib/contant/constant.dart
@@ -1,14 +1,22 @@
-import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter/services.dart' show ByteData, rootBundle;
 import 'package:pdf/widgets.dart' as pw;
 
 final DateTime todayDateTime = DateTime.now();
 
 Future<pw.Font> fontBold() async {
-    final fontData = await rootBundle.load("fonts/Bitter-Bold.ttf");
-    return pw.Font.ttf(fontData);
-  }
-  Future<pw.Font> fontMedium() async {
-    final fontData = await rootBundle.load("fonts/Bitter-Medium.ttf");
-    return pw.Font.ttf(fontData);
-  }
+  final fontData = await rootBundle.load('assets/fonts/Bitter-Bold.ttf');
+  final buffer = fontData.buffer;
+  final byteData = ByteData.view(buffer);
+  return pw.Font.ttf(byteData);
+}
+
+Future<pw.Font> fontMedium() async {
+  final fontData = await rootBundle.load('assets/fonts/Bitter-Medium.ttf');
+  final buffer = fontData.buffer;
+  final byteData = ByteData.view(buffer);
+  return pw.Font.ttf(byteData);
+}
+
+
+
 
